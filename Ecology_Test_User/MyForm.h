39bc3731bae5b2ca44109cbb076770e65366a579
Антон Новиков <novikov.asn15@gmail.com>
2023-11-16 20:34:38 +0300

    Tabs autosize, testing checkbox

--- Ecology_Test_User/MyForm.h
+++ Ecology_Test_User/MyForm.h
@@ -34,10 +34,16 @@ namespace Ecology_Test_User {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::TabControl^ tabControl1;
+	private: System::Windows::Forms::TabControl^ tabs;
 	protected:
+
+	protected:
+
 	private: System::Windows::Forms::TabPage^ tabPage1;
-	private: System::Windows::Forms::TabPage^ tabPage2;
+	private: System::Windows::Forms::CheckedListBox^ checkedListBox1;
+	protected:
+
+
 
 	private:
 		/// <summary>
@@ -53,63 +59,75 @@ namespace Ecology_Test_User {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
-			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabs = (gcnew System::Windows::Forms::TabControl());
 			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
-			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
-			this->tabControl1->SuspendLayout();
+			this->checkedListBox1 = (gcnew System::Windows::Forms::CheckedListBox());
+			this->tabs->SuspendLayout();
+			this->tabPage1->SuspendLayout();
 			this->SuspendLayout();
 			// 
-			// tabControl1
+			// tabs
 			// 
-			this->tabControl1->Controls->Add(this->tabPage1);
-			this->tabControl1->Controls->Add(this->tabPage2);
-			this->tabControl1->Location = System::Drawing::Point(24, 12);
-			this->tabControl1->Multiline = true;
-			this->tabControl1->Name = L"tabControl1";
-			this->tabControl1->SelectedIndex = 0;
-			this->tabControl1->Size = System::Drawing::Size(200, 100);
-			this->tabControl1->TabIndex = 0;
+			this->tabs->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->tabs->Controls->Add(this->tabPage1);
+			this->tabs->Location = System::Drawing::Point(0, 0);
+			this->tabs->Multiline = true;
+			this->tabs->Name = L"tabs";
+			this->tabs->SelectedIndex = 0;
+			this->tabs->Size = System::Drawing::Size(724, 461);
+			this->tabs->TabIndex = 0;
 			// 
 			// tabPage1
 			// 
+			this->tabPage1->AutoScroll = true;
+			this->tabPage1->Controls->Add(this->checkedListBox1);
 			this->tabPage1->Location = System::Drawing::Point(4, 22);
 			this->tabPage1->Name = L"tabPage1";
 			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage1->Size = System::Drawing::Size(192, 74);
-			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Size = System::Drawing::Size(716, 435);
+			this->tabPage1->TabIndex = 2;
 			this->tabPage1->Text = L"tabPage1";
 			this->tabPage1->UseVisualStyleBackColor = true;
 			// 
-			// tabPage2
+			// checkedListBox1
 			// 
-			this->tabPage2->Location = System::Drawing::Point(4, 22);
-			this->tabPage2->Name = L"tabPage2";
-			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage2->Size = System::Drawing::Size(192, 74);
-			this->tabPage2->TabIndex = 1;
-			this->tabPage2->Text = L"tabPage2";
-			this->tabPage2->UseVisualStyleBackColor = true;
+			this->checkedListBox1->FormattingEnabled = true;
+			this->checkedListBox1->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"Вариант 1", L"Вариант 2", L"Вариант 3" });
+			this->checkedListBox1->Location = System::Drawing::Point(46, 133);
+			this->checkedListBox1->Name = L"checkedListBox1";
+			this->checkedListBox1->Size = System::Drawing::Size(552, 244);
+			this->checkedListBox1->TabIndex = 0;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
-			this->Controls->Add(this->tabControl1);
+			this->AutoScroll = true;
+			this->ClientSize = System::Drawing::Size(724, 461);
+			this->Controls->Add(this->tabs);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"MyForm";
 			this->Text = L"Тест по экологии";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->SizeChanged += gcnew System::EventHandler(this, &MyForm::MyForm_SizeChanged);
-			this->tabControl1->ResumeLayout(false);
+			this->tabs->ResumeLayout(false);
+			this->tabPage1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		tabs->Height = this->Height - 49;
+		tabs->Top= 5;
+		tabs->Left = 5;
+		tabs->Width = this->Width - 26;
+	}
+	
 	private: System::Void MyForm_SizeChanged(System::Object^ sender, System::EventArgs^ e) {
-		tabControl1->Left = 0;
-		tabControl1->Top = 0;
-		tabControl1->Width = this->Width;
-
+		tabs->Height = this->Height - 49;
+		tabs->Top = 5;
+		tabs->Left = 5;
+		tabs->Width = this->Width - 26;
 	}
 	};
 }
