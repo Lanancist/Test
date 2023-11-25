bb54ee2f43d4f5825d196930033d0aa59f4d9eae
Антон Новиков <novikov.asn15@gmail.com>
2023-11-25 21:57:01 +0300

    C# project added

---
+++ User_Module/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace User_Module
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabs = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+			this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.tabs.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tabs
+			// 
+			this.tabs.Controls.Add(this.tabPage1);
+			this.tabs.Controls.Add(this.tabPage2);
+			this.tabs.Location = new System.Drawing.Point(517, 194);
+			this.tabs.Name = "tabs";
+			this.tabs.SelectedIndex = 0;
+			this.tabs.Size = new System.Drawing.Size(200, 100);
+			this.tabs.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(192, 74);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(192, 74);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "Exctl|*.xls";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// файлToolStripMenuItem
+			// 
+			this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+			this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+			this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+			this.файлToolStripMenuItem.Text = "Файл";
+			// 
+			// открытьToolStripMenuItem
+			// 
+			this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+			this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+			this.открытьToolStripMenuItem.Text = "Открыть";
+			this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripComboBox1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+			this.toolStrip1.TabIndex = 2;
+			this.toolStrip1.Text = "Лист";
+			// 
+			// toolStripLabel1
+			// 
+			this.toolStripLabel1.Name = "toolStripLabel1";
+			this.toolStripLabel1.Size = new System.Drawing.Size(33, 22);
+			this.toolStripLabel1.Text = "Лист";
+			// 
+			// toolStripComboBox1
+			// 
+			this.toolStripComboBox1.Name = "toolStripComboBox1";
+			this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+			this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 49);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(800, 401);
+			this.dataGridView1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.tabs);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tabs.ResumeLayout(false);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabs;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+		private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
+
---
+++ User_Module/Form1.cs
@@ -0,0 +1,133 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace User_Module
+{
+	public partial class Form1 : Form
+	{
+		private string filename = string.Empty;
+		private DataTableCollection tableCollection = null;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		TabPage[] tabPages;
+		int n = 5;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			tabs.BringToFront();
+			tabs.Dock = DockStyle.Fill;
+			tabPages = new TabPage[n + 1];
+			for (int i = 0; i < n; i++)
+			{
+				tabPages[i] = new TabPage();
+				tabPages[i].Text = "Вопрос " + (i + 1);
+				tabPages[i].BackColor = Color.White;
+				tabPages[i].AutoScroll = true;
+				tabs.Controls.Add(tabPages[i]);
+				SplitContainer splitter = new SplitContainer();
+				tabPages[i].Controls.Add(splitter);
+				splitter.Parent = this;
+				splitter.Name = "splitter" + i;
+				splitter.Visible = true;
+				splitter.Enabled = true;
+				splitter.Dock = DockStyle.Fill;
+				splitter.Orientation = Orientation.Horizontal;
+				CheckedListBox answers = new CheckedListBox();
+				splitter.Panel2.Controls.Add(answers);
+				splitter.Parent = tabPages[i];
+				answers.Name = "Check" + i;
+				answers.Visible = true;
+				answers.Dock = DockStyle.Fill;
+				splitter.SplitterDistance = 236;
+				answers.BeginUpdate();
+				for (int j = 0; j < 6; j++)
+					answers.Items.Add("Вариант" + (j + 1), (j + 1) % 2 == 1);
+				answers.EndUpdate();
+				TextBox quest = new TextBox();
+				splitter.Panel1.Controls.Add(quest);
+				splitter.Name = "Text" + i;
+				splitter.Text = "Text" + i;
+				quest.Dock = DockStyle.Fill;
+				quest.Multiline = true;
+				quest.ReadOnly = true;
+				quest.ScrollBars = ScrollBars.Vertical;
+				quest.ShortcutsEnabled = false;
+				quest.TabIndex = 0;
+			}
+			tabPages[n] = new TabPage();
+			tabPages[n].BackColor = Color.White;
+			tabPages[n].Text = "Окончить тест";
+			tabPages[n].AutoScroll = true;
+			tabs.Controls.Add(tabPages[n]);
+			Button btn = new Button();
+			tabPages[n].Controls.Add(btn);
+			btn.Name = "btnEnd";
+			btn.Text = "Завершить тест";
+			btn.Visible = true;
+			btn.Enabled = true;
+			btn.Top = (this.Height - 100) / 2;
+			btn.Left = (this.Width - 200) / 2;
+			btn.Width = 200;
+			btn.Height = 100;
+
+		}
+
+		private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			tabs.Visible = false;
+			try
+			{
+				DialogResult res = openFileDialog1.ShowDialog();
+				if (res == DialogResult.OK)
+				{
+					filename = openFileDialog1.FileName;
+					Text = filename;
+					OpenExcelFile(filename);
+				}
+				else throw new Exception("Файл не выбран");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		private void OpenExcelFile(string path)
+		{
+			FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read);
+			IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+			DataSet db = reader.AsDataSet(new ExcelDataSetConfiguration()
+			{
+				ConfigureDataTable = (x) => new ExcelDataTableConfiguration()
+				{
+					UseHeaderRow = true
+				}
+			});
+			tableCollection = db.Tables;
+			toolStripComboBox1.Items.Clear();
+			foreach (DataTable table in tableCollection)
+			{
+				toolStripComboBox1.Items.Add(table.TableName);
+			}
+			toolStripComboBox1.SelectedIndex = 0;
+		}
+
+		private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			DataTable table = tableCollection[Convert.ToString(toolStripComboBox1.SelectedIndex)];
+			dataGridView1.DataSource = table;
+		}
+	}
+
+}
+
