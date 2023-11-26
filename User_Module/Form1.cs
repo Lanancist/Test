aebccc935c124b2df0bb547eb5779cdf3a6f4019
Антон Новиков <novikov.asn15@gmail.com>
2023-11-26 21:10:50 +0300

    random done

--- User_Module/Form1.cs
+++ User_Module/Form1.cs
@@ -20,14 +20,27 @@ namespace User_Module
 		}
 		//TabPage[] tabPages;
 		int n, generateCount;
-		bool password, isNamed, fromEveryTheme;
+		bool password, isNamed, fromEveryTheme, obligateQuestions;
 
 		private void button1_Click(object sender, EventArgs e)
 		{
 			tabs.Visible = true;
 		}
+		public void GenerateRandomNumbers(int n, int a, int b, ref List<int> num)
+		{
+			Random rand = new Random();
+			int k = 0, c;
+			while (k < n)
+			{
+				c = rand.Next(a, b + 1);
+				if (num.Contains(c))
+					continue;
+				num.Add(c);
+				k++;
+			}
 
-		
+		}
+
 
 		private void Form1_ResizeEnd(object sender, EventArgs e)
 		{
@@ -62,11 +75,11 @@ namespace User_Module
 			string lastChanged = File.GetLastWriteTime(path).ToString();
 			lastChanged = lastChanged.Replace(".", "").Replace(" ", "").Replace(":", "");
 			line = fin.ReadLine();
-			string[] questm=line.Split(new[] { '.' }, StringSplitOptions.None); ;
+			string[] questm = line.Split(new[] { '.' }, StringSplitOptions.None); ;
 			password = questm[0] == "1";
 			isNamed = questm[1] == "1";
 			fromEveryTheme = questm[2] == "1";
-			//obligateQuestions = questm[3] == "1";
+			obligateQuestions = questm[3] == "1";
 			/*
 			 * if (lastChanged.Length<14) lastChanged.Insert(9, "0");
 			 */
@@ -82,26 +95,69 @@ namespace User_Module
 				return;
 			}
 			generateCount = int.Parse(questm[5]);
+			List<int> numbers = new List<int>();
+			if (fromEveryTheme)
+			{
+				if (obligateQuestions)
+				{
+					int k = int.Parse(questm[7]) - 1, a;
+					for (int i = 8; i < questm.Length; i++)
+					{
+						a = int.Parse(questm[i]);
+						GenerateRandomNumbers(generateCount, k + 1, k + a, ref numbers);
+						k += a;
+					}
+				}
+				else
+				{
+					int k = 0, a;
+					for (int i = 7; i < questm.Length; i++)
+					{
+						a = int.Parse(questm[i]);
+						GenerateRandomNumbers(generateCount, k + 1, k + a, ref numbers);
+						k += a;
+					}
+				}
+
+			}
+			else
+			{
+				if (obligateQuestions)
+				{
+					int a = int.Parse(questm[7]) - 1;
+					GenerateRandomNumbers(generateCount, a + 1, n, ref numbers);
+				}
+				else
+					GenerateRandomNumbers(generateCount, 0, n, ref numbers);
+			}
 
 			n = int.Parse(questm[6]);
-			Text = fin.ReadLine();
-			label1.Text = Text;
+			if (isNamed)
+			{
+				Text = fin.ReadLine();
+				label1.Text = Text;
+			}
+
 			TabPage tabPages;
+			int questnumb=0;
 			for (int i = 0; i < n; i++)
 			{
+				if (numbers.Contains(i))
+					continue;
+				++questnumb;
 				line = fin.ReadLine();
 				line = line.Remove(0, 1);
 				line = line.Remove(line.Length - 1, 1);
 				questm = line.Split(new[] { "\";\"" }, StringSplitOptions.None);
 				tabPages = new TabPage();
-				tabPages.Text = "Вопрос " + (i + 1);
+				tabPages.Text = "Вопрос " + (questnumb + 1);
 				tabPages.BackColor = Color.White;
 				tabPages.AutoScroll = true;
 				tabs.TabPages.Add(tabPages);
 				SplitContainer splitter = new SplitContainer();
 				tabPages.Controls.Add(splitter);
 				splitter.Parent = this;
-				splitter.Name = "splitter" + i;
+				splitter.Name = "splitter" + questnumb;
 				splitter.Visible = true;
 				splitter.Enabled = true;
 				splitter.Dock = DockStyle.Fill;
@@ -109,8 +165,8 @@ namespace User_Module
 				splitter.Parent = tabPages;
 				splitter.SplitterDistance = 236;
 				Label label1 = new Label();
-				label1.Name = "Label" + i;
-				label1.Text = questm[2] + '\n' + "Вопрос " + (i + 1);
+				label1.Name = "Label" + questnumb;
+				label1.Text = questm[2] + '\n' + "Вопрос " + (questnumb + 1);
 				label1.Dock = DockStyle.Top;
 				label1.Height = 40;
 				label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -118,7 +174,7 @@ namespace User_Module
 				tabPages.Controls.Add(label1);
 				RichTextBox quest = new RichTextBox();
 				splitter.Panel1.Controls.Add(quest);
-				quest.Name = "Text" + i;
+				quest.Name = "Text" + questnumb;
 				quest.Text = questm[2];
 				quest.Top = 100;
 				quest.Dock = DockStyle.Fill;
@@ -134,7 +190,7 @@ namespace User_Module
 					CheckedListBox answers = new CheckedListBox();
 					splitter.Panel2.Controls.Add(answers);
 					answers.BorderStyle = BorderStyle.None;
-					answers.Name = "Check" + i;
+					answers.Name = "Check" + questnumb;
 					answers.Visible = true;
 					answers.Dock = DockStyle.Fill;
 					answers.BeginUpdate();
@@ -157,7 +213,7 @@ namespace User_Module
 							radioButton = new RadioButton();
 							radioButton.AutoSize = true;
 							radioButton.Location = new System.Drawing.Point(10, 23 * j);
-							radioButton.Name = "radioButton" + i;
+							radioButton.Name = "radioButton" + questnumb;
 							radioButton.Size = new System.Drawing.Size(85, 17);
 							radioButton.TabIndex = j;
 							radioButton.TabStop = true;
