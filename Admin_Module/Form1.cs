a56ba38d5ca251c1d26a7bdb3d91561a5caeb919
Антон Новиков <novikov.asn15@gmail.com>
2023-11-28 22:18:54 +0300

    input and export work

--- Admin_Module/Form1.cs
+++ Admin_Module/Form1.cs
@@ -10,6 +10,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using ExcelDataReader;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
 namespace Admin_Module
 {
 	public partial class Form1 : Form
@@ -71,7 +75,6 @@ namespace Admin_Module
 				MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 		}
-
 		private void toolStripButton2_Click(object sender, EventArgs e)
 		{
 			dataGridView1.Visible = true;
@@ -79,7 +82,6 @@ namespace Admin_Module
 			toolStripButton3.Enabled = true;
 			toolStripButton2.Enabled = false;
 		}
-
 		private void toolStripButton3_Click(object sender, EventArgs e)
 		{
 			dataGridView1.Visible = false;
@@ -107,23 +109,82 @@ namespace Admin_Module
 		string path = "input.txt";
 		private void button1_Click(object sender, EventArgs e)
 		{
-			dataGridView1.Sort(this.dataGridView1.Columns[0], ListSortDirection.Ascending);
-			StreamWriter fout = new StreamWriter(path, false);
-			string s="";
-			int a = radioButton3.Checked? 1:0;
-			s += a+".1.";
-			a=radioButton2.Checked? 1: 0;
-			s += a + ".";
-			string lastChanged = DateTime.Now.ToString();
-			lastChanged = lastChanged.Replace(".", "").Replace(" ", "").Replace(":", "");
-			Text = s;
-			fout.Close();
+			//dataGridView1.Sort(this.dataGridView1.Columns[0], ListSortDirection.Ascending);
+			//StreamWriter fout = new StreamWriter(path, false);
+			//string s="";
+			//int a = radioButton3.Checked? 1:0;
+			//s += a+".1.";
+			//a=radioButton2.Checked? 1: 0;
+			//s += a + ".";
+			//string lastChanged = DateTime.Now.ToString();
+			//lastChanged = lastChanged.Replace(".", "").Replace(" ", "").Replace(":", "");
+			//Text = s;
+			//fout.Close();
+
+			Cursor.Current = Cursors.WaitCursor;
+			Excel.Application xlApp = new Excel.Application();
+			Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\data.xls");
+			Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+			Excel.Range xlRange = xlWorksheet.UsedRange;
+
+			xlWorksheet.Cells[1, 1] = Text;
+			xlApp.Visible = false;
+			xlApp.UserControl = false;
+			xlWorkbook.Save();
+
+			//cleanup
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+
+			//release com objects to fully kill excel process from running in the background
+			Marshal.ReleaseComObject(xlRange);
+			Marshal.ReleaseComObject(xlWorksheet);
+
+			//close and release
+			xlWorkbook.Close();
+			Marshal.ReleaseComObject(xlWorkbook);
+
+			//quit and release
+			xlApp.Quit();
+			Marshal.ReleaseComObject(xlApp);
+
+			// Set cursor as default arrow
+			Cursor.Current = Cursors.Default;
 		}
 
 		private void toolStripButton4_Click(object sender, EventArgs e)
 		{
-			
-			
+			Cursor.Current = Cursors.WaitCursor;
+
+			Excel.Application xlApp = new Excel.Application();
+			Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\data.xls");
+			Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+			Excel.Range xlRange = xlWorksheet.UsedRange;
+
+			if (xlRange.Cells[1, 1] != null && xlRange.Cells[1, 1].Value2 != null)
+			{
+				Text = "New "+xlRange.Cells[1, 1].Value2.ToString();
+			}
+
+			//cleanup
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+
+			//release com objects to fully kill excel process from running in the background
+			Marshal.ReleaseComObject(xlRange);
+			Marshal.ReleaseComObject(xlWorksheet);
+
+			//close and release
+			xlWorkbook.Close();
+			Marshal.ReleaseComObject(xlWorkbook);
+
+			//quit and release
+			xlApp.Quit();
+			Marshal.ReleaseComObject(xlApp);
+
+			// Set cursor as default arrow
+			Cursor.Current = Cursors.Default;
+
 		}
 	}
 }
